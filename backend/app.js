900331de6f532851391369ad7770b411278c6ebe
remonroy <remonroy34@gmail.com>
2022-12-15 13:46:02 +0600

    change code

--- backend/app.js
+++ backend/app.js
@@ -20,9 +20,21 @@ const errorMiddleware = require("./middleWare/error");
 app.use("/api/v1", loggingRouter);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
+
+//New code add
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
 });
+//New code end
+
 //error handling use
 app.use(errorMiddleware);
 
